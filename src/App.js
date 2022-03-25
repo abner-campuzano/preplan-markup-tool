7586932f26d7a5079494dc195aa800074c222771
abner-campuzano <abner.campuzano@southmetro.org>
2022-03-25 17:28:31 -0600

    yarn

--- src/App.js
+++ src/App.js
@@ -7,7 +7,8 @@ import * as api_client from './services/api_client';
 export default function App() {
   const queryParams = queryString.parse(window.location.search);
 
-  //const documentURL = api_client.get_preplan(queryParams.preplanId);
+  var documentURLOLD = api_client.get_preplan(queryParams.preplanId);
+  documentURLOLD = documentURLOLD +"";
   const documentURL = `https://preplanios.blob.core.windows.net/preplans/${queryParams.preplanId}.pdf?sp=r&st=2022-03-25T21:41:56Z&se=2032-03-26T05:41:56Z&spr=https&sv=2020-08-04&sr=c&sig=XrhOgs7oZS7aFuDHpTNlg0xlZOnTZ5oVJY%2BX6qakmq4%3D`;
 
   console.log(documentURL);
