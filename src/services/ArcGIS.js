a2dcedcbe0902d1b3ed969e10284d6f9692a3e59
abner.campuzano@southmetro.org <abner.camppuzano@southmetro.org>
2022-04-19 16:17:38 -0600

    aadd as parameters

--- src/services/ArcGIS.js
+++ src/services/ArcGIS.js
@@ -9,10 +9,8 @@ const ArcGISHelper = {
         console.log(blob);
         const formData = new FormData();
         formData.append("attachment", blob);
-        formData.append("multipart", true);
-        formData.append("filename", "test.pdf");
         fetch(
-            "https://gis.southmetro.org/arcgis/rest/services/Hosted/assignments_b77065eb42c84954bf9b4897a2b042b1/FeatureServer/0/10443/addAttachment?token=VR-2ox-RBiMunH8s7muYUB3BUa8cUB9jWaXmdMp8LorEKVQD4jL0Jqa2O2AmcNTeq8eKNc5L42On9erA0PClE2k24mq0GyeH32W0XloF9flQ44tRAz6sAmtiBL5gBSWuG2NNVXQrCKWXYTZxEIhyg3i3wpBBtabIErdEr4PmFYYIUzNF2doiKIRzXp947MwMDOsEkCumd7YOaNdIa_u0yA..&f=json",
+            "https://gis.southmetro.org/arcgis/rest/services/Hosted/assignments_b77065eb42c84954bf9b4897a2b042b1/FeatureServer/0/10443/addAttachment?multipart=true&filename=test.pdf&token=VR-2ox-RBiMunH8s7muYUB3BUa8cUB9jWaXmdMp8LorEKVQD4jL0Jqa2O2AmcNTeq8eKNc5L42On9erA0PClE2k24mq0GyeH32W0XloF9flQ44tRAz6sAmtiBL5gBSWuG2NNVXQrCKWXYTZxEIhyg3i3wpBBtabIErdEr4PmFYYIUzNF2doiKIRzXp947MwMDOsEkCumd7YOaNdIa_u0yA..&f=json",
             {
                 method: "POST",
                 body: formData
