68671ad19adffa0cfbfb2d7786f4c60c8b4efa6a
abner.campuzano@southmetro.org <abner.camppuzano@southmetro.org>
2022-04-19 11:06:15 -0600

    Test 4

--- src/services/ArcGIS.js
+++ src/services/ArcGIS.js
@@ -4,9 +4,9 @@ const ArcGISHelper = {
 
 
         const arrayBuffer = await instance.exportPDF();
+        console.log(instance);
         const blob = new Blob([arrayBuffer], { type: 'application/pdf' });
         const formData = new FormData();
-        formData.append("multipart", "true");
         formData.append("attachment", blob);
         fetch(
             "https://gis.southmetro.org/arcgis/rest/services/Hosted/assignments_b77065eb42c84954bf9b4897a2b042b1/FeatureServer/0/10443/addAttachment?token=A79jprp3Zmholb_3KuSECmeF8WcV3vtzfCdq0Uhm2TM3PAay_142NqDDOS6uEfpiVctDsPo9LEFB_n2O76bLcEUTAFZnbEhlgn45Q8QBpgMtw_jFxvLijhKRNEIeWZWCbs6YcxTYGNxb9nC-KeH5wyNRLT7oITojcQRMhZekp3pk_6Gw-i333LziMdwdEKhg&f=json", {
