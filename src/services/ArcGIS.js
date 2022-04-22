06622c1ab8944bb04028f0a05b6a30d6068831ae
abner-campuzano <abner.campuzano@southmetro.org>
2022-04-22 14:10:49 -0600

    new token

--- src/services/ArcGIS.js
+++ src/services/ArcGIS.js
@@ -8,7 +8,7 @@ const ArcGISHelper = {
         const blob = new Blob([arrayBuffer], { type: 'application/pdf' });
         console.log(blob);
         const formData = new FormData();
-        formData.append("token", "VR-2ox-RBiMunH8s7muYUB3BUa8cUB9jWaXmdMp8LorEKVQD4jL0Jqa2O2AmcNTeq8eKNc5L42On9erA0PClE2k24mq0GyeH32W0XloF9flQ44tRAz6sAmtiBL5gBSWuG2NNVXQrCKWXYTZxEIhyg3i3wpBBtabIErdEr4PmFYYIUzNF2doiKIRzXp947MwMDOsEkCumd7YOaNdIa_u0yA..&");
+        formData.append("token", "UhKW5huFWlgSaFDp550RiMI5oCfET5qdrFtC_3ljzgKvFLOqrcVHhsHw1aC3dvHoGTzRe16_m9VBTZ5ILNRdKFBGVPVFl1E2uUFzxwS_eUUio4TONYlup3qiq3maN-RELTZqP4mkC1UpXYb2Ls39xlrtHrekUbS9n3Aj3nIhu0LMMHegy405CQTcCwDRxpX9NsnQCeKwTwVCe66yjmjwZt9b7H54Zwxi6XKCMat48eGQ2aEiwiDyjf134X0tyR0p");
         formData.append("attachment", blob);
         fetch(
             "https://gis.southmetro.org/arcgis/rest/services/Hosted/assignments_b77065eb42c84954bf9b4897a2b042b1/FeatureServer/0/10443/addAttachment?f=json",
