1fce39469c1bf9124d8401b77b219c79d5d49403
abner-campuzano <abner.campuzano@southmetro.org>
2022-04-25 14:05:54 -0600

    URLSearchParams

--- src/services/ArcGIS.js
+++ src/services/ArcGIS.js
@@ -32,12 +32,12 @@ const ArcGISHelper = {
     attatchPDFtoAssignment: async function (instance, objectId) {
 
         console.log("objectId=" + objectId);
-
+        
         const arrayBuffer = await instance.exportPDF();
         console.log(arrayBuffer);
         const blob = new Blob([arrayBuffer], { type: 'application/pdf' });
         console.log(blob);
-        const formData = new FormData();
+        const formData = new URLSearchParams();
         formData.append("token", await this.getToken());
         //formData.append("token", "UhKW5huFWlgSaFDp550RiMI5oCfET5qdrFtC_3ljzgKvFLOqrcVHhsHw1aC3dvHoGTzRe16_m9VBTZ5ILNRdKFBGVPVFl1E2uUFzxwS_eUUio4TONYlup3qiq3maN-RELTZqP4mkC1UpXYb2Ls39xlrtHrekUbS9n3Aj3nIhu0LMMHegy405CQTcCwDRxpX9NsnQCeKwTwVCe66yjmjwZt9b7H54Zwxi6XKCMat48eGQ2aEiwiDyjf134X0tyR0p");
         formData.append("attachment", blob);
@@ -46,6 +46,9 @@ const ArcGISHelper = {
             `https://gis.southmetro.org/arcgis/rest/services/Hosted/workforce_9bce7612ad40407881aefb4d6ced6232/FeatureServer/0/${objectId}/addAttachment`,
             {
                 method: "POST",
+                headers: {
+                    'Content-Type': 'application/x-www-form-urlencoded'
+                  }, 
                 referrerPolicy:"origin",
                 body: formData
             }).then(response => console.log(response.json()));
