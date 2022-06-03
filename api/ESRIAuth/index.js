83f69b81834936e15d0619887b718d1221d13ffd
abner.campuzano@southmetro.org <abner.camppuzano@southmetro.org>
2022-06-03 11:34:44 -0600

    try

--- api/ESRIAuth/index.js
+++ api/ESRIAuth/index.js
@@ -1,22 +1,22 @@
 module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
 
-    // const formData = new URLSearchParams();
-    // formData.append("username", "1-GIS-Service@southmetro");
-    // formData.append("password", "GIS9195Smfr");
-    // formData.append("client", "referer");
-    // formData.append("referer", "https://ambitious-sand-0bf74c810.1.azurestaticapps.net");
-    // formData.append("expiration", "10160")
-    // formData.append("f", "json");
-    // var response = await fetch(
-    //     `https://gis.southmetro.org/portal/sharing/rest/generateToken`,
-    //     {
-    //         method: "POST",
-    //         headers: {
-    //             'Content-Type': 'application/x-www-form-urlencoded'
-    //         },
-    //         body: formData
-    //     });
+    const formData = new URLSearchParams();
+    formData.append("username", "1-GIS-Service@southmetro");
+    formData.append("password", "GIS9195Smfr");
+    formData.append("client", "referer");
+    formData.append("referer", "https://ambitious-sand-0bf74c810.1.azurestaticapps.net");
+    formData.append("expiration", "10160")
+    formData.append("f", "json");
+    var response = await fetch(
+        `https://gis.southmetro.org/portal/sharing/rest/generateToken`,
+        {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded'
+            },
+            body: formData
+        });
 
     // var jsonResponse = await response.json();
     // //token = jsonResponse.token;
@@ -24,6 +24,6 @@ module.exports = async function (context, req) {
     // console.log(jsonResponse);
 
     context.res.json({
-        text: "Hello from the API"
+        text: response
     });
 }
