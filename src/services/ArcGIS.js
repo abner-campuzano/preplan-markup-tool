13b4f4fa11eb81c8e2358bf41b3d907562a23cec
abner.campuzano@southmetro.org <abner.camppuzano@southmetro.org>
2022-06-03 14:36:29 -0600

    aaaa

--- src/services/ArcGIS.js
+++ src/services/ArcGIS.js
@@ -1,7 +1,7 @@
 const ArcGISHelper = {
 
     getToken: async function () {
-        const { response } = await (await fetch(`/api/ESRIAuth`)).json();
+        const { response } = await fetch(`/api/ESRIAuth`);
         console.log("token");
         console.log(response);
 
