d8a8a42ef0f411120b3261bde079f81577026aef
abner-campuzano <abner.campuzano@southmetro.org>
2023-12-28 11:28:26 -0700

    pspdfkit api fix

--- api/PSPDFKit/index.js
+++ api/PSPDFKit/index.js
@@ -1,4 +1,4 @@
-module.exports = async function (context, req) {
+export default async function (context, req) { 
     context.log('JavaScript HTTP trigger function processed a request.');
    
    
